68d76e59db22066179f8140c9f7df028d86e9501
Mahmoud Montaser <hicishicis112233@gmail.com>
2026-05-17 19:37:40 +0300

    chore:add controllers files

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(helmet()); 
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
 
 
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcrypt"
+import isEmail from "validator/lib/isEmail.js";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -10,18 +11,28 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true,
         unique: true,
+        validate(val) {
+            if (!isEmail(val)) { throw new Error("emai is invalid") }
+        }
+
 
 
     },
     password: {
-        type:String,
+        type: String,
         required: true,
         trim: true,
+        validate(val) {
+            const password = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+            if (!password.test(val)) { throw new Error("password should be capital,small ,numbers and special characters") }
+
+        }
+
 
 
     },
-    refresh_token: { type: String,default:null },
-    team: {   type: mongoose.Schema.Types.ObjectId,ref: "Team" },
+    refresh_token: { type: String, default: null },
+    team: { type: mongoose.Schema.Types.ObjectId, ref: "Team" },
 
 
 
